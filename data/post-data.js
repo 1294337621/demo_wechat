61be78f4c802262125151f94babd3a90b593c2ca
1294337621 <1294337621@qq.com>
2019-06-11 18:32:50 +0800

    模板使用

---
+++ data/post-data.js
@@ -0,0 +1,24 @@
+var post_content = [
+  {
+    date: 'Nov 25 2016',
+    title: '直击顶峰礼包',
+    imgsrc: '/images/post/content_1.jpg',
+    avatar: '/images/post/avatar_1.jpg',
+    content: '两个都是售价8888点劵，只能用点劵',
+    reading: 66,
+    collection: 66
+  },
+  {
+    date: 'Nov 26 2016',
+    title: '直击顶峰礼包1',
+    imgsrc: '/images/post/content_1.jpg',
+    avatar: '/images/post/avatar_1.jpg',
+    content: '两个都是售价8888点劵，只能用点劵',
+    reading: 66,
+    collection: 66
+  }
+];
+
+module.exports = {
+  post_content
+}
--- pages/posts/post.js
+++ pages/posts/post.js
@@ -1,3 +1,5 @@
+var postsData = require('../../data/post-data.js')
+
 Page({
 
   /**
@@ -11,27 +13,8 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var post_content = [
-      {
-        date: 'Nov 25 2016',
-        title: '直击顶峰礼包',
-        post_img: '/images/post/content_1.jpg',
-        author_img: '/images/post/avatar_1.jpg',
-        content: '两个都是售价8888点劵，只能用点劵',
-        view_num: 66, 
-        collect_num: 66
-      },
-      {
-        date: 'Nov 26 2016',
-        title: '直击顶峰礼包1',
-        post_img: '/images/post/content_1.jpg',
-        author_img: '/images/post/avatar_1.jpg',
-        content: '两个都是售价8888点劵，只能用点劵',
-        view_num: 66,
-        collect_num: 66
-      }
-    ];
-
-    this.setData({post_content});
+    this.setData({
+      post_content: postsData.post_content
+    });
   }
 })
